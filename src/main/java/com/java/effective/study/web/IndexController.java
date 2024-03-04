f2d8f0c6a3bdf2688ce641acadaaa5137c4c865f
jun <alswns1201@gmail.com>
2024-03-04 10:19:23 +0900

    [4장]: mustache 메인idex 만들어보기. (스프링 2.7.x이후 부터는 한글 강제 설정 appilcation.properties 적용)

---
+++ src/main/java/com/java/effective/study/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.java.effective.study.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping("/") // 초기 화면
+    public String index(){
+        return "index"; // resources/templates/index를 화면에 return
+    }
+
+}
