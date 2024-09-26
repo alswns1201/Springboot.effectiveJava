f05b241fea986ddb3c05af504032f4489f42bbf1
jun <alswns1201@gmail.com>
2024-09-26 09:42:52 +0900

    연습 코딩 분리 정리

--- src/test/java/generic/Main.java
+++
@@ -1,9 +0,0 @@
-package generic;
-
-public class Main {
-    public static void main(String [] args){
-        ComparableBox<Integer> box = new ComparableBox<>();
-        box.setItem(10);
-        System.out.print(box.isGreaterThan(4));
-    }
-}
--- src/test/java/pattern/Singletone/Beverage.java
+++
@@ -1,4 +0,0 @@
-package pattern.Singletone;
-
-public class Beverage {
-}
